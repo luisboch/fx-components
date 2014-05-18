b8bcb865f83384ef8f1ca7449261b4fc1b24eb32
Luis <Luis@Luis-PC>
2014-05-18 14:07:37 -0300

    Ajuste nas mensagens

--- src/main/java/org/luis/fx/components/Message.java
+++ src/main/java/org/luis/fx/components/Message.java
@@ -9,6 +9,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Pane;
 import org.luis.fx.components.message.Type;
 
@@ -20,28 +21,16 @@ public class Message extends Pane {
 
     private static final Logger log = Logger.getLogger(Message.class.getName());
 
-    private static final Image error;
-    private static final Image info;
-    private static final Image success;
-    private static final Image warn;
-
-    static {
-        error = new Image(Message.class.getResource("/image/error_32.png").getPath());
-        info = new Image(Message.class.getResource("/image/info_32.png").getPath());
-        success = new Image(Message.class.getResource("/image/success_32.png").getPath());
-        warn = new Image(Message.class.getResource("/image/warning_32.png").getPath());
-    }
-
     @FXML
     private Label lblMsg;
 
     @FXML
-    private ImageView imgIcon;
+    private FlowPane flowPaneImgIcon;
 
     public Message(final String msg, final Type type) {
 
         URL location = getClass().getResource("/fxml/Message.fxml");
-        
+
         FXMLLoader loader = new FXMLLoader(location);
 
         loader.setController(this);
@@ -58,23 +47,33 @@ public class Message extends Pane {
     }
 
     private void set(String msg, Type type) {
-        
+
         lblMsg.setText(msg);
-        
-        switch (type) {
-            case ERROR:
-                imgIcon.setImage(error);
-                break;
-            case INFO:
-                imgIcon.setImage(info);
-                break;
-            case SUCCESS:
-                imgIcon.setImage(success);
-                break;
-            case WARNING:
-                imgIcon.setImage(warn);
-                break;
+        if (type != Type.INFO) {
+            flowPaneImgIcon.getChildren().clear();
+            switch (type) {
+                case ERROR:
+                    final Image error = new Image(Message.class.getResource("/image/error_32.png").getPath());
+                    final ImageView imgError = new ImageView(error);
+                    flowPaneImgIcon.getChildren().add(imgError);
+                    break;
+                case SUCCESS:
+                    final Image success = new Image(Message.class.getResource("/image/success_32.png").getPath());
+                    final ImageView imgSuccess = new ImageView(success);
+                    flowPaneImgIcon.getChildren().add(imgSuccess);
+                    break;
+                case WARNING:
+                    final Image warn = new Image(Message.class.getResource("/image/warning_32.png").getPath());
+                    final ImageView imgWarn = new ImageView(warn);
+                    flowPaneImgIcon.getChildren().add(imgWarn);
+                    break;
+
+                default:
+                    final Image info = new Image(Message.class.getResource("/image/info_32.png").getPath());
+                    final ImageView imgInfo = new ImageView(info);
+                    flowPaneImgIcon.getChildren().add(imgInfo);
+                    break;
+            }
         }
-        
     }
 }
--- src/main/java/org/luis/fx/components/Messages.java
+++ src/main/java/org/luis/fx/components/Messages.java
@@ -1,30 +1,27 @@
 package org.luis.fx.components;
 
-import java.util.Queue;
-import java.util.concurrent.ConcurrentLinkedDeque;
-import javafx.animation.Animation;
 import javafx.animation.FadeTransition;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.util.Duration;
 import org.luis.fx.components.message.Type;
 
 public class Messages extends Pane {
 
-    private final HBox hbox = new HBox();
+    private final VBox hbox = new VBox();
 
     public Messages() {
         super();
 
         hbox.setSpacing(10d);
         getChildren().add(hbox);
-        
+
         this.setVisible(false);
-        
+
         hbox.setPrefHeight(getHeight());
         hbox.setPrefWidth(getWidth());
     }
@@ -34,25 +31,28 @@ public class Messages extends Pane {
     }
 
     public void showMessage(final String msg, final Type type) {
-        
+
         final Messages myself = this;
-        
+
         myself.setVisible(true);
-        
+
         final Message message = new Message(msg, type);
-        
+
         final FadeTransition startFade = new FadeTransition(Duration.millis(200), message);
         startFade.setFromValue(0d);
         startFade.setToValue(1d);
         startFade.play();
+        
         hbox.getChildren().add(message);
+
+        updateHeigth(message);
 
         Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(30), "showing", new EventHandler<ActionEvent>() {
 
             @Override
             public void handle(ActionEvent event) {
 
-                FadeTransition finalFade = new FadeTransition(Duration.millis(200), message);
+                final FadeTransition finalFade = new FadeTransition(Duration.millis(200), message);
                 finalFade.setFromValue(1);
                 finalFade.setToValue(0);
                 finalFade.play();
@@ -63,6 +63,7 @@ public class Messages extends Pane {
                     public void handle(ActionEvent event) {
                         message.setVisible(false);
                         hbox.getChildren().remove(message);
+                        updateHeigth(message);
                         if (hbox.getChildren().isEmpty()) {
                             myself.setVisible(false);
                         }
@@ -74,5 +75,20 @@ public class Messages extends Pane {
 
         timeline.play();
 
+    }
+
+    private void updateHeigth(Message m) {
+
+        if (hbox.getChildren().isEmpty()) {
+            hbox.setMinHeight(0d);
+            this.setMinHeight(0d);
+            return;
+        }
+
+        final Double height = (m.getPrefHeight() + hbox.getSpacing()) * hbox.getChildren().size();
+
+        hbox.setMinHeight(height);
+        this.setMinHeight(height);
+        
     }
 }
